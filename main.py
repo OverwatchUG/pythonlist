ccb475ee06194a81e847c3a5d6defb7af1e95cca
OverwatchUG <shadiduwaako@gmail.com>
2023-09-27 18:47:27 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# names_string = input("Give me everybody's name separated by a comma. ")
+# names = names_string.split(", ")
+#
+# import random
+# # x = len(names)
+# # random_number = random.randint(0, x - 1)
+# # print(random_number)
+# # person = names[random_number]
+#
+# person = random.choice(names)
+# print(person + " " + "is going to pay the bill today")
+
+# dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+# print(len(dirty_dozen))
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale",  "Tomatoes", "Celery", "Potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen)
